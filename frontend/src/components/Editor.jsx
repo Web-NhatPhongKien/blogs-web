e7e3d956c30bf2c504919f1f3bca8e78f9d60720
kiennarutonbmn1 <kiennarutonb1@gmail.com>
2026-03-31 15:44:57 +0700

    trung kien commit

---
+++ frontend/src/components/Editor.jsx
@@ -0,0 +1,20 @@
+import EditorJS from "@editorjs/editorjs";
+import { useEffect, useRef } from "react";
+
+export default function Editor({ setContent }) {
+  const editorRef = useRef();
+
+  useEffect(() => {
+    editorRef.current = new EditorJS({
+      holder: "editorjs",
+      async onChange() {
+        const data = await editorRef.current.save();
+        setContent(data);
+      }
+    });
+
+    return () => editorRef.current.destroy();
+  }, []);
+
+  return <div id="editorjs"></div>;
+}
