7776d6444d68f78b2aec2e6f528975ed4a491ff6
kiennarutonbmn1 <kiennarutonb1@gmail.com>
2026-05-17 22:44:38 +0700

    Fix conflict and update blog editor UI

--- frontend/src/components/publish-form.jsx
+++ frontend/src/components/publish-form.jsx
@@ -1,13 +1,7 @@
 const PublishForm = () => {
-<<<<<<< HEAD
-    return ( <div>Publish Form</div> )
-}
-export default PublishForm; 
-=======
     return (
         <div>Trang đăng bài</div>
     );
 }
 
 export default PublishForm;
->>>>>>> b751743 (Update editor, publish form and banner)
